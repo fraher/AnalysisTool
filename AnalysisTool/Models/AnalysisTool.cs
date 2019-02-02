4f13f6c64410ccbc36ce85e072f643ec35140421
= <fraher@gmail.com>
2019-02-02 17:25:06 -0600

    Initial Check in

---
+++ AnalysisTool/Models/AnalysisTool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AnalysisTool.Models
+{
+
+    public class AnalysisToolContext : DbContext
+    {
+        public AnalysisToolContext(DbContextOptions<AnalysisToolContext> options)
+            : base(options)
+        { }
+
+        
+    }
+
+}
--- AnalysisTool/Startup.cs
+++ AnalysisTool/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AnalysisTool.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnalysisTool
 {
@@ -29,10 +31,13 @@ namespace AnalysisTool
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
+            });            
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=FishDreams;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<AnalysisToolContext>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
