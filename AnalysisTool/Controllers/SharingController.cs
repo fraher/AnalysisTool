2209752a01261a196dc9670994be24153fe4f0bf
= <fraher@gmail.com>
2019-02-02 19:23:21 -0600

    Updated the template and added partial view to assessments page.

---
+++ AnalysisTool/Controllers/SharingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnalysisTool.Controllers
+{
+    public class SharingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
