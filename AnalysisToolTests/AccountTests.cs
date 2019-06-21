abbaae838b383671afc04dd6d47cb45fb678e918
Christopher Fraher <fraher@gmail.com>
2019-06-20 22:43:59 -0500

    Updated naming to tests.

---
+++ AnalysisToolTests/AccountTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using Moq;
+using AnalysisTool.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AnalysisToolTests
+{
+    public class AccountTests
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
